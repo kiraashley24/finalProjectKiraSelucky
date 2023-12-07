5de139848161b1ccf675961006ec30f7652e515c
kiraashley24 <kiraselucky@gmail.com>
2023-12-07 10:37:22 -0700

    added index as initial page

--- finalProjectKiraSelucky/urls.py
+++ finalProjectKiraSelucky/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from dnd.views import index
 
 urlpatterns = [
     path("dnd/", include("dnd.urls", namespace='dnd')),
     path("admin/", admin.site.urls),
+    re_path(r"^$", index, name="index"),
 ]
