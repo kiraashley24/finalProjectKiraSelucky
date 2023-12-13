2e9ecaa8b44352ae9c1e5502b49f3cb703488324
kiraashley24 <kiraselucky@gmail.com>
2023-12-13 10:22:03 -0700

    styling adjustments

--- dnd/views.py
+++ dnd/views.py
@@ -1,12 +1,11 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render, get_object_or_404
 from .models import Race, DndClass, BarbarianDetail, BardDetail, ClericDetail, DruidDetail, FighterDetail, MonkDetail, PaladinDetail, RangerDetail, RogueDetail, SorcererDetail, WarlockDetail, WizardDetail
 import requests
 from .forms import CharacterForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from requests.exceptions import HTTPError, RequestException, JSONDecodeError
-from django.contrib.auth import login, logout
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
@@ -129,15 +128,17 @@ def register(request):
 
     return render(request, 'registration/register.html', {'form': form})
 
+
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('charbuild.html') # redirects to charbuild page on successful login
+            return redirect('charbuild')  # or any other success URL
         else:
-            messages.error(request, 'Login failed. Please check your username and password.') # unsuccessful message
+            messages.add_message(request, messages.ERROR, 'Invalid Credentials. Please check your username and password.')
+
 
     else:
         form = AuthenticationForm()
